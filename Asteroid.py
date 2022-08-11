64583d3addbf89e8fa15955faf675d800337b594
Adrien <110352057+AdrienLD09@users.noreply.github.com>
2022-08-11 17:00:04 +0000

    Finished Game

--- Asteroid.py
+++ Asteroid.py
@@ -9,15 +9,15 @@ class AsteroidClass(pygame.sprite.Sprite):
     self.rect.center = pos
     self.speed = pygame.math.Vector2(0,3)
     self.speed.rotate_ip(random.randint(0,360))
-def movement(self):
-  screen_info = pygame.display.Info()
-  self.rect.move_ip(self.speed)
-  if self.rect.left <= 0 or self.rect.right >= screen_info.current_w:
-    self.speed[0] *= - 1
-    self.image = pygame.transform.flip(self.image, True, False)
-    self.rect.move_ip(self.speed[0],0)
-  if self.rect.bottom <= 0 or self.rect.top >= screen_info.current_h:
-    self.speed[1] *= - 1
-    self.image = pygame.transform.flip(self.image, False, True)
-    self.rect.move_ip(0,self.speed[1])
-  
+  def movement(self):
+    screen_info = pygame.display.Info()
+    self.rect.move_ip(self.speed)
+    if self.rect.left <= 0 or self.rect.right >= screen_info.current_w:
+      self.speed[0] *= - 1
+      self.image = pygame.transform.flip(self.image, True, False)
+      self.rect.move_ip(self.speed[0],0)
+    if self.rect.bottom <= 0 or self.rect.top >= screen_info.current_h:
+      self.speed[1] *= - 1
+      self.image = pygame.transform.flip(self.image, False, True)
+      self.rect.move_ip(0,self.speed[1])
+    
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #importing librarys
-import pygame, sys, random
+import pygame, sys, random, pandas as pd
 from pygame.locals import*
 from Asteroid import AsteroidClass
 from Ship import ShipClass
 #initializing/setup
 pygame.init()
+
+
 
 screen_info = pygame.display.Info()
 width = int(screen_info.current_w)
@@ -12,33 +14,40 @@ height = int(screen_info.current_h)
 size = (width,height)
 screen = pygame.display.set_mode(size)
 
+
+
 clock = pygame.time.Clock()
 color = (30,0,30)
 screen.fill(color)
+
+df = pd.read_csv("game_info.csv")
+
 #variables
-numlevel = 8
-Level = 1
-player = ShipClass((20,200))
+numlevel = df['LevelNum'].max()
+Level = df['LevelNum'].min()
+LevelData = df.iloc[Level]
 asteroids = pygame.sprite.Group()
-asteroidCount = 2
+asteroidCount = LevelData['AsteroidCount']
+player = ShipClass((LevelData['PlayerX'], LevelData['PlayerY']))
 
 def win():
-  font = pygame.font.SysFont(none,70)
+  font = pygame.font.SysFont(None,70)
   text = font.render("You Win!!!", True, (255,0,0))
   text_rec = text.get_rect()
   text_rec.center = (width/2, height/2)
   while True:
     screen.fill(color)
-    screen.blit
+    screen.blit(text,text_rec)
+    pygame.display.flip()
 
 
 def init():
-  global asteroidCount
+  global asteroidCount , LevelData
   global asteroids
-
+  LevelData = df.iloc[Level]
   player.reset ((20,200))
   asteroids.empty()
-  asteroidCount += 3
+  asteroidCount += 2
   for i in range(asteroidCount):
     asteroids.add(AsteroidClass((random.randint(50, width - 50),random.randint(50, height - 50)), random.randint(15, 60)))
     
@@ -76,14 +85,22 @@ def main():
     player.movement() 
     for asteroid in asteroids:
       asteroid.movement()
+    get_hit = pygame.sprite.spritecollide(player,asteroids, False)
     asteroids.draw(screen)
     screen.blit(player.image,player.rect)
     pygame.display.flip()
 
     if player.checkReset(width):
-      init()
-
+      if Level == numlevel:
+        break
+      else:
+        Level+=1
+        init()
+    elif get_hit:
+      player.reset((LevelData['PlayerX'],LevelData['PlayerY']))
+      player.reset((LevelData['PlayerX'],LevelData['PlayerY']))
       #mainloop
   win()
 if __name__ == "__main__":
   main()
+
